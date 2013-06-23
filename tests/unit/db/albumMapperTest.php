493a4a549a7eca45d262c844c56a00aea7474341
Morris Jobke <morris.jobke@gmail.com>
2013-06-23 19:20:11 +0200

    {artist|album}MapperTests

---
+++ tests/unit/db/albumMapperTest.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+* ownCloud - Music
+*
+* @author Morris Jobke
+* @copyright 2013 Morris Jobke morris.jobke@gmail.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+namespace OCA\Music\Db;
+
+require_once(__DIR__ . "/../../classloader.php");
+
+
+class AlbumMapperTest extends \OCA\AppFramework\Utility\MapperTestUtility {
+
+	private $mapper;
+	private $albums;
+
+	private $userId = 'john';
+	private $id = 5;
+	private $row;
+
+	public function setUp()
+	{
+		$this->beforeEach();
+
+		$this->mapper = new AlbumMapper($this->api);
+
+		// create mock items
+		$album1 = new Album();
+		$album1->setName('Test name');
+		$album1->setYear(2013);
+		$album1->setCover('http://example.org');
+		$album1->resetUpdatedFields();
+
+		$this->albums = array(
+			$album1
+		);
+
+		$this->row = array(
+			array('id' => $this->albums[0]->getId(), 'name' => 'Test name', 'year' => 2013, 'cover' => 'http://example.org'),
+		);
+
+	}
+
+
+	private function makeSelectQuery($condition=null){
+		return 'SELECT `album`.`name`, `album`.`year`, `album`.`id`, '.
+			'`album`.`cover` '.
+			'FROM `*PREFIX*music_albums` `album` '.
+			'WHERE `album`.`user_id` = ? ' . $condition;
+	}
+
+	public function testFind(){
+		$sql = $this->makeSelectQuery('AND `album`.`id` = ?');
+		$this->setMapperResult($sql, array($this->userId, $this->id), $this->row);
+		$result = $this->mapper->find($this->id, $this->userId);
+		$this->assertEquals($this->albums[0], $result);
+	}
+}
---
+++ tests/unit/db/artistMapperTest.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+* ownCloud - Music
+*
+* @author Morris Jobke
+* @copyright 2013 Morris Jobke morris.jobke@gmail.com
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+* License as published by the Free Software Foundation; either
+* version 3 of the License, or any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+*
+* You should have received a copy of the GNU Affero General Public
+* License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+namespace OCA\Music\Db;
+
+require_once(__DIR__ . "/../../classloader.php");
+
+
+class ArtistMapperTest extends \OCA\AppFramework\Utility\MapperTestUtility {
+
+	private $mapper;
+	private $artists;
+
+	private $userId = 'john';
+	private $id = 5;
+	private $row;
+
+	public function setUp()
+	{
+		$this->beforeEach();
+
+		$this->mapper = new ArtistMapper($this->api);
+
+		// create mock items
+		$artist1 = new Artist();
+		$artist1->setName('Test name');
+		$artist1->setImage('http://example.org');
+		$artist1->resetUpdatedFields();
+
+		$this->artists = array(
+			$artist1
+		);
+
+		$this->row = array(
+			array('id' => $this->artists[0]->getId(), 'name' => 'Test name', 'image' => 'http://example.org'),
+		);
+
+	}
+
+
+	private function makeSelectQuery($condition=null){
+		return 'SELECT `artist`.`name`, `artist`.`image`, `artist`.`id`'.
+			'FROM `*PREFIX*music_artists` `artist` '.
+			'WHERE `artist`.`user_id` = ? ' . $condition;
+	}
+
+	public function testFind(){
+		$sql = $this->makeSelectQuery('AND `artist`.`id` = ?');
+		$this->setMapperResult($sql, array($this->userId, $this->id), $this->row);
+		$result = $this->mapper->find($this->id, $this->userId);
+		$this->assertEquals($this->artists[0], $result);
+	}
+}
